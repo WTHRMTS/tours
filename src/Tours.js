dfc65aef07fd4df69077fa18c5afd6504c25ca50
malediction99@gmail.com <malediction99@gmail.com>
2021-11-09 12:58:00 +1100

    complete functionality

--- src/Tours.js
+++ src/Tours.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = ({tours}) => {
+const Tours = ({tours, removeTour}) => {
   console.log({tours});
   return (
   <section>
@@ -10,7 +10,7 @@ const Tours = ({tours}) => {
       </div>
       <div>
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour}></Tour>
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />
         })
       }
     </div>
